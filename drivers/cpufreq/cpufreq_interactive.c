8417c4a9be6bf1f6499ad69196eb354ef9da2052
bemerguy <heiler.bemerguy@gmail.com>
2024-06-08 17:57:18 -0300

    Tweak Bandido governor again

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -75,11 +75,11 @@ static struct mutex gov_lock;
 
 /* Target load.  Lower values result in higher CPU speeds. */
 #define DEFAULT_TARGET_LOAD 90
-static unsigned int default_target_loads[] = { 70, 1200000, 90 };
+static unsigned int default_target_loads[] = { 90, 1200000, 100 };
 
 #define DEFAULT_TIMER_RATE (10 * USEC_PER_MSEC)
 #define DEFAULT_ABOVE_HISPEED_DELAY DEFAULT_TIMER_RATE
-static unsigned int default_above_hispeed_delay[] = { 10000, 1200000, 6000 };
+static unsigned int default_above_hispeed_delay[] = { 10000, 1200000, 30000 };
 
 struct cpufreq_interactive_tunables {
 	int usage_count;
@@ -96,7 +96,7 @@ struct cpufreq_interactive_tunables {
 	 * The minimum amount of time to spend at a frequency before we can ramp
 	 * down.
 	 */
-#define DEFAULT_MIN_SAMPLE_TIME (10 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (30 * USEC_PER_MSEC)
 	unsigned long min_sample_time;
 	/*
 	 * The sample rate of the timer used to increase frequency
