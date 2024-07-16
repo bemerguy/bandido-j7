64f83e9d28cacd74cb098382e22223e139889bc7
bemerguy <heiler.bemerguy@gmail.com>
2024-07-16 18:52:42 -0300

    build_kernel.sh upgrade

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#rm log.txt
-#make clean
-#make distclean
-#export CROSS_COMPILE=/home/tien.tq1/toolchains/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export ARCH=arm64
+#Bandido kernel build script. fbs @ xda
 
-make exynos7580-a5xelte_defconfig
-make -j
-#24 2>&1 | tee -a  log.txt
+#Set CROSS_COMPILE to your gcc path.
+#It should end in the last "-" of the gcc's binary full path
+export CROSS_COMPILE=/home/me/x-tools/14.1forsize/bin/aarch64-linux-gnu-
+
+export ARCH=arm64
+DATE_START=$(date +"%s")
+IMAGE="out/arch/arm64/boot/Image"
+DTB="out/arch/arm64/boot/dtb.img"
+
+mkdir -p out
+rm $IMAGE &> /dev/null
+
+CPU=$(($(nproc) - 1))
+
+make -C $(pwd) O=$(pwd)/out bandido_defconfig
+make -j$CPU -C $(pwd) O=$(pwd)/out |tee ../bandj7.log
+
+if [[ -f "$IMAGE" ]] && [[ -f "$DTB" ]]; then
+        STATE=`adb get-state`
+        if [[ $STATE != "recovery" ]]; then
+                adb reboot recovery
+        fi
+        KERNELZIP="bandido-kernel-$(date +"%Y%m%d%H%M").zip"
+
+        rm AnyKernel3/*Image* > /dev/null 2>&1
+        rm AnyKernel3/*.zip > /dev/null 2>&1
+	rm AnyKernel3/dt* > /dev/null 2>&1
+
+        cp $IMAGE AnyKernel3
+	cp $DTB AnyKernel3/dt
+        cd AnyKernel3
+
+        zip -r9 $KERNELZIP .
+
+        DATE_END=$(date +"%s")
+        DIFF=$(($DATE_END - $DATE_START))
+
+        echo -e "\nTime elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\n"
+        adb wait-for-recovery
+        adb push $KERNELZIP /sdcard/
+else
+        echo -e "ERROR\n"
+fi
